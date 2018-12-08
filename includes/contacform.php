4f42dd1ff14c27e1ea38c5e2d3cf563bc2bff953
unknown <550356048xyc@gmail.com>
2018-12-08 16:59:53 +0800

    update

--- includes/contacform.php
+++ includes/contacform.php
@@ -8,8 +8,8 @@ $message = $_POST['message'];
 
 
 // $mailTo = "azusakaworu@gmail.com";
-$mailTo = "l_ding5@fanshaweonline.ca";
-$headers ="From: noreply@lingdingportfolio.ca". "\r\n";
+$mailTo = "y_xia59790@fanshaweonline.ca";
+$headers ="From: noreply@yuchenxia.com". "\r\n";
 $headers .='Reply-To:'.$mailFrom;
 $txt = "You have received an e-mail from".$name.".\n\n".$message;
 
--- js/app.js
+++ js/app.js
@@ -1,77 +1,100 @@
-
-
-
 // do not remove this line
 
 // write your own JS here:
 
 (() => {
-  $(document).foundation()
+  $(document).foundation();
 
-var imageBanner = document.querySelector("#houseImages"),
-  sigils = document.querySelectorAll(".sigilContainer"),
-  lightbox = document.querySelector('.lightbox'),
-  closeLBox = lightbox.querySelector('.close-lightbox'),
-  vidPlayer = lightbox.querySelector('video'),
-  offSet = 600,
-  currentSource,
-  houseName = 'demoreel',
-  theExt = '.mp4';
+  var 
+    lightbox = document.querySelector(".lightbox"),
+      closeLBox = lightbox.querySelector(".close-lightbox"),
+      sigils = document.querySelectorAll(".sigilContainer"),
+    vidPlayer = lightbox.querySelector("video"),
+    offSet = 600,
+    currentSource,
+    houseName = "demoreel",
+    theExt = ".mp4";
 
   //(1) add array for taglines, replace text with each animation
 
-imageBanner.style.right = "0px";
+
+  //functions
+
+  function closeLightBox() {
+    //(2) complete this function
+    lightbox.classList.remove("show-lightbox");
+    //vidPlayer.currentTime = 0; //rewinds video to start
+  }
 
 
-//functions
+  function showHouseVideo() {
+    currentSource = vidPlayer.currentSrc;
+    console.log(currentSource);
+    theExt = currentSource.substr(currentSource.lastIndexOf("."));
+    vidPlayer.src = "video/house-" + houseName + theExt;
+    lightbox.classList.add("show-lightbox");
+    vidPlayer.load();
+    vidPlayer.play();
+  }
 
-function closeLightBox() {
-//(2) complete this function
-lightbox.classList.remove('show-lightbox');
-//vidPlayer.currentTime = 0; //rewinds video to start
+  //events and listeners
 
-}
+  closeLBox.addEventListener("click", closeLightBox);
 
+  for (var i = 0; i < sigils.length; i++) {
+    sigils[i].addEventListener("click", moveBanner, false);
+  }
 
-function moveBanner() {
-  imageBanner.style.right = (offSet * this.dataset.offset) + 'px';
-  //set houseName to the name of the second class on this item
-  houseName = this.className.split(' ')[1];
+  //(4) when video finishes playing, also close the lightbox
+  vidPlayer.addEventListener("ended", closeLightBox);
 
-  //(3) add listener for end of css transition, use 'transitionend' event
-  //    to open the lightbox and start playing the selected video
-  setTimeout("showHouseVideo()",600);
-  //showHouseVideo();
-}
+  var xmlhttp;
+  function loadXMLDoc(url) {
+    xmlhttp = null;
+    if (window.XMLHttpRequest) {
+      // code for all new browsers
+      xmlhttp = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+      // code for IE5 and IE6
+      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    if (xmlhttp != null) {
+      xmlhttp.onreadystatechange = state_Change;
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send(null);
+    } else {
+    }
+  }
 
+  function state_Change() {
+    if (xmlhttp.readyState == 4) {
+      if (xmlhttp.status == 200) {
+        console.log(xmlhttp);
+      } else {
+        console.log(xmlhttp);
+      }
+    }
+  }
 
-function showHouseVideo() {
-  currentSource = vidPlayer.currentSrc;
-  console.log(currentSource);
-  theExt = currentSource.substr(currentSource.lastIndexOf("."));
-  vidPlayer.src = 'video/house-'+houseName+theExt;
-  lightbox.classList.add('show-lightbox');
-  vidPlayer.load();
-  vidPlayer.play();
-}
+  loadXMLDoc("http://www.google.com");
 
+  $(".mask-animation").on("click", function() {
+    var img = $(this)
+      .find("img")
+      .attr("src");
+    $("#lightboximg").attr("src", img);
+    $("#lightbox").css({ display: "block" });
+    setTimeout(() => {
+      $("#lightbox")
+        .removeClass("lightbox-show")
+        .addClass("lightbox-show");
+    });
+  });
 
-//events and listeners
-
-closeLBox.addEventListener('click', closeLightBox);
-
-for(var i =0; i < sigils.length; i++) {
-  sigils[i].addEventListener('click', moveBanner, false);
-}
-
-//(4) when video finishes playing, also close the lightbox
-vidPlayer.addEventListener('ended',closeLightBox);
-  
-  
-  
-
-
-  
-
-
+  $(".lightbox-close").on("click", function() {
+    $("#lightbox").removeClass("lightbox-show");
+    setTimeout(() => {
+      $("#lightbox").css({ display: "none" });
+    }, 300);
+  });
 })();
